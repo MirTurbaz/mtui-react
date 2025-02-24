a818553ed3b14ca0765db0eb6c52028eebe957a5
jojogis <jojogis.lesha@yandex.ru>
2025-02-24 13:08:16 +0300

    components 11

--- src/components/common/tooltip.tsx
+++ src/components/common/tooltip.tsx
@@ -5,6 +5,7 @@ import { createPortal } from 'react-dom';
 import { useResize } from '../../hooks/use_resize';
 import Close from '../icons/close';
 import { Button } from './button';
+import { TypePopupPlacement } from './popup';
 
 export interface TooltipProps {
   className?: string;
@@ -13,6 +14,8 @@ export interface TooltipProps {
   children?: ReactElement | ReactElement[] | string | number;
   animated?: boolean;
   onChange?: (value: boolean) => void;
+  placement?: TypePopupPlacement;
+  isLeaveOnClick?: boolean;
 }
 
 export const Tooltip: React.FC<TooltipProps> = (props) => {
@@ -23,7 +26,7 @@ export const Tooltip: React.FC<TooltipProps> = (props) => {
   const [clicked, setClicked] = useState(false);
 
   const handleClick = () => {
-    let newClicked = !clicked;
+    let newClicked = !clicked && !props.isLeaveOnClick;
     setClicked(newClicked);
     setIsOpen(newClicked);
     props.onChange(newClicked);
@@ -42,6 +45,43 @@ export const Tooltip: React.FC<TooltipProps> = (props) => {
       props.onChange(false);
     }
   };
+
+  const calculateTooltipPosition = (anchor: HTMLElement | null,
+                                    placement: TypePopupPlacement = 'bottom',
+                                    ref: HTMLElement | null): [number, number] => {
+    let left = 0;
+    let top = 0;
+
+    if (!anchor || !ref) return [ left, top ];
+
+    const anchorRect = anchor.getBoundingClientRect();
+    const tooltipRect = ref.getBoundingClientRect();
+    const scrollY = window.scrollY;
+
+    const vertical = ['top', 'bottom'].includes(placement);
+
+    if (vertical) {
+      left = anchorRect.left + anchorRect.width / 2 - tooltipRect.width / 2;
+    } else {
+      top = anchorRect.top + (anchorRect.height / 2 - tooltipRect.height / 2) + scrollY;
+    }
+
+    if (placement.includes('right')) {
+      left = anchorRect.left + anchorRect.width;
+    } else if (placement.includes('left')) {
+      left = anchorRect.left - tooltipRect.width;
+    } else if (placement.includes('top')) {
+      top = anchorRect.top - tooltipRect.height + scrollY;
+    } else {
+      top = anchorRect.top + anchorRect.height + scrollY;
+    }
+
+    if (left < 0) left = 18;
+    if (top < 0) top = 10;
+    if (left + tooltipRect.width > window.innerWidth) left = window.innerWidth - tooltipRect.width  - 18;
+
+    return [left, top];
+  }
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
@@ -65,52 +105,42 @@ export const Tooltip: React.FC<TooltipProps> = (props) => {
 
   const renderTooltip = () => {
     let left, top;
-    if (anchor) {
-      const bbox = anchor.getBoundingClientRect();
-      const tooltipWidth = ref?.getBoundingClientRect().width ?? 0;
-
-      left = bbox.left + bbox.width / 2 - tooltipWidth / 2;
-      if (left + tooltipWidth > window.innerWidth) left = window.innerWidth - tooltipWidth - 18;
-      if (left < 0) left = 18;
-
-      top = bbox.top + bbox.height + window.scrollY;
-      if (top < 0) top = 10;
-    }
+    [ left, top ] = calculateTooltipPosition(anchor, props.placement, ref);
 
     return createPortal(
-      <div
-        ref={setRef}
-        className={`tooltip__wrapper ${props.className} ${props.animated === false ? 'no-animation' : ''}`}
-        onClick={(e) => e.stopPropagation()}
-        style={{ left, top }}
-      >
-        {isMobile ? (
-          <>
-            <div className={'tooltip__header'}>
-              <div className={'tooltip__title'}>{props.title}</div>
-              <Button
-                onClick={() => {
-                  setClicked(false);
-                  setIsOpen(false);
-                }}
-              >
-                <Close />
-              </Button>
-            </div>
-            <div className={'tooltip__body'}>{props.content}</div>
-          </>
-        ) : (
-          <div className={'tooltip__body'}>{props.content}</div>
-        )}
-      </div>,
-      document.body
+        <div
+            ref={setRef}
+            className={`tooltip__wrapper ${props.className} ${props.animated === false ? 'no-animation' : ''}`}
+            onClick={(e) => e.stopPropagation()}
+            style={{ left, top }}
+        >
+          {isMobile ? (
+              <>
+                <div className={'tooltip__header'}>
+                  <div className={'tooltip__title'}>{props.title}</div>
+                  <Button
+                      onClick={() => {
+                        setClicked(false);
+                        setIsOpen(false);
+                      }}
+                  >
+                    <Close />
+                  </Button>
+                </div>
+                <div className={'tooltip__body'}>{props.content}</div>
+              </>
+          ) : (
+              <div className={'tooltip__body'}>{props.content}</div>
+          )}
+        </div>,
+        document.body
     );
   };
 
   return (
-    <div ref={setAnchor} onClick={handleClick} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-      {props.children}
-      {isOpen && renderTooltip()}
-    </div>
+      <div ref={setAnchor} onClick={handleClick} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        {props.children}
+        {isOpen && renderTooltip()}
+      </div>
   );
 };
