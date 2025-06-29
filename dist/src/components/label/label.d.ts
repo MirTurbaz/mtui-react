597fba9bc9dba2f2aff56b56ba7014295cf54598
CENTURION <n.centurion.d@gmail.com>
2025-06-29 13:46:33 +0300

    Label refactor

---
+++ dist/src/components/label/label.d.ts
@@ -0,0 +1,3 @@
+import * as React from 'react';
+import { ILabelProps } from './types';
+export declare const Label: React.FC<ILabelProps>;
---
+++ src/components/label/types.ts
@@ -0,0 +1,8 @@
+import { ReactElement, ReactNode } from 'react';
+
+export interface ILabelProps {
+  color?: 'red' | 'orange' | 'blue' | 'green';
+  children: ReactNode;
+  icon?: ReactElement;
+  className?: string;
+}
